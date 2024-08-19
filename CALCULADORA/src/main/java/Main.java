5b73863c0822d070def395e59a4bf3f6c81184ff
DERS0214 <scadrami@espol.edu.ec>
2024-08-19 16:00:05 -0500

    Calculadora Base

---
+++ CALCULADORA/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora c = new Calculadora();
+        System.out.println(c.division(10,-2));
+    }
+    
+}
---
+++ CALCULADORA/src/test/java/MainTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CltControl
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Main.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
