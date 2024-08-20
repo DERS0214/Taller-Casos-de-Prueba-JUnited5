0e8958124323afdd2e73ee7c0e056b13dd41f762
DAVID <scadrami@espol.edu.ec>
2024-08-19 23:32:11 -0500

    varios

--- CALCULADORA/src/test/java/CalculadoraTest.java
+++ CALCULADORA/src/test/java/CalculadoraTest.java
@@ -28,7 +28,7 @@ public class CalculadoraTest {
 
     @Test
     public void TCS01() {
-        System.out.println("suma");
+        System.out.println("TCS01");
         double a = -2;
         double b = -2;
         Calculadora instance = new Calculadora();
@@ -39,7 +39,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCS02() {
-        System.out.println("suma");
+        System.out.println("TCS02");
         double a = 0;
         double b = 0;
         Calculadora instance = new Calculadora();
@@ -50,7 +50,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCS03() {
-        System.out.println("suma");
+        System.out.println("TCS03");
         double a = 2.5;
         double b = 3.6;
         Calculadora instance = new Calculadora();
@@ -61,7 +61,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCS04() {
-        System.out.println("suma");
+        System.out.println("TCS04");
         double a = 1000000;
         double b = 2000000;
         Calculadora instance = new Calculadora();
@@ -72,7 +72,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCS05() {
-        System.out.println("suma");
+        System.out.println("TCS05");
         double a = 7;
         double b = -2;
         Calculadora instance = new Calculadora();
@@ -83,7 +83,7 @@ public class CalculadoraTest {
 
     @Test
     public void TCR01() {
-        System.out.println("resta");
+        System.out.println("TCR01");
         double a = 10;
         double b = 5;
         Calculadora instance = new Calculadora();
@@ -94,7 +94,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCR02() {
-        System.out.println("resta");
+        System.out.println("TCR02");
         double a = -10;
         double b = -5;
         Calculadora instance = new Calculadora();
@@ -105,7 +105,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCR03() {
-        System.out.println("resta");
+        System.out.println("TCR03");
         double a = 0;
         double b = 0;
         Calculadora instance = new Calculadora();
@@ -116,7 +116,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCR04() {
-        System.out.println("resta");
+        System.out.println("TCR04");
         double a = 3.5;
         double b = 1.2;
         Calculadora instance = new Calculadora();
@@ -127,7 +127,7 @@ public class CalculadoraTest {
 
     @Test
     public void TCM01() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM01");
         double a = 4;
         double b = 6;
         Calculadora instance = new Calculadora();
@@ -138,7 +138,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCM02() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM02");
         double a = -7;
         double b = 5;
         Calculadora instance = new Calculadora();
@@ -149,7 +149,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCM03() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM03");
         double a = -6;
         double b = -6;
         Calculadora instance = new Calculadora();
@@ -160,7 +160,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCM04() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM04");
         double a = 0;
         double b = 2;
         Calculadora instance = new Calculadora();
@@ -171,7 +171,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCM05() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM05");
         double a = 0.5;
         double b = 0.2;
         Calculadora instance = new Calculadora();
@@ -182,7 +182,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCM06() {
-        System.out.println("multiplicacion");
+        System.out.println("TCM06");
         double a = 10000;
         double b = 0.01;
         Calculadora instance = new Calculadora();
@@ -193,7 +193,7 @@ public class CalculadoraTest {
 
     @Test
     public void TCD01() {
-        System.out.println("division");
+        System.out.println("TCD01");
         double a = 10;
         double b = 2;
         Calculadora instance = new Calculadora();
@@ -204,7 +204,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCD02() {
-        System.out.println("division");
+        System.out.println("TCD02");
         double a = 10;
         double b = -2;
         Calculadora instance = new Calculadora();
@@ -215,7 +215,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCD03() {
-        System.out.println("division");
+        System.out.println("TCD03");
         double a = 0;
         double b = 25;
         Calculadora instance = new Calculadora();
@@ -226,7 +226,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCD04() {
-        System.out.println("division");
+        System.out.println("TCD04");
         double a = 108;
         double b = 0;
         Calculadora instance = new Calculadora();
@@ -237,7 +237,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCD05() {
-        System.out.println("division");
+        System.out.println("TCD05");
         double a = 25;
         double b = 10;
         Calculadora instance = new Calculadora();
@@ -249,7 +249,7 @@ public class CalculadoraTest {
     // EXPONENCIACIÓN
     @Test
     public void TCE01() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE01");
         double base = 2;
         double exponente = 3;
         Calculadora instance = new Calculadora();
@@ -260,7 +260,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE02() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE02");
         double base = 2;
         double exponente = -3;
         Calculadora instance = new Calculadora();
@@ -271,7 +271,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE03() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE03");
         double base = -2;
         double exponente = 3;
         Calculadora instance = new Calculadora();
@@ -282,7 +282,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE04() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE04");
         double base = 2;
         double exponente = 0;
         Calculadora instance = new Calculadora();
@@ -293,7 +293,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE05() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE05");
         double base = 0;
         double exponente = 2;
         Calculadora instance = new Calculadora();
@@ -304,7 +304,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE06() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE06");
         double base = 2;
         double exponente = 0.5;
         Calculadora instance = new Calculadora();
@@ -315,7 +315,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCE07() {
-        System.out.println("exponenciacion");
+        System.out.println("TCE07");
         double base = 2;
         double exponente = 1/2;
         Calculadora instance = new Calculadora();
@@ -327,7 +327,7 @@ public class CalculadoraTest {
     //RADICACIÓN
     @Test
     public void TCRD01() {
-        System.out.println("radicacion");
+        System.out.println("TCRD01");
         double numero = 16;
         double indice = 2;
         Calculadora instance = new Calculadora();
@@ -338,7 +338,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD02() {
-        System.out.println("radicacion");
+        System.out.println("TCRD02");
         double numero = 27;
         double indice = 3;
         Calculadora instance = new Calculadora();
@@ -349,7 +349,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD03() {
-        System.out.println("radicacion");
+        System.out.println("TCRD03");
         double numero = -16;
         double indice = 2;
         Calculadora instance = new Calculadora();
@@ -360,7 +360,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD04() {
-        System.out.println("radicacion");
+        System.out.println("TCRD04");
         double numero = 0;
         double indice = 2;
         Calculadora instance = new Calculadora();
@@ -371,7 +371,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD05() {
-        System.out.println("radicacion");
+        System.out.println("TCRD05");
         double numero = 81;
         double indice = 4;
         Calculadora instance = new Calculadora();
@@ -382,7 +382,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD06() {
-        System.out.println("radicacion");
+        System.out.println("TCRD06");
         double numero = 10;
         double indice = 2.5;
         Calculadora instance = new Calculadora();
@@ -393,7 +393,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD07() {
-        System.out.println("radicacion");
+        System.out.println("TCRD07");
         double numero = 10.5;
         double indice = 2;
         Calculadora instance = new Calculadora();
@@ -404,7 +404,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCRD08() {
-        System.out.println("radicacion");
+        System.out.println("TCRD08");
         double numero = 10.5;
         double indice = 0;
         Calculadora instance = new Calculadora();
@@ -415,7 +415,7 @@ public class CalculadoraTest {
     //NUMEROS AMIGOS
     @Test
     public void TNA01() {
-        System.out.println("sonAmigos");
+        System.out.println("TNA01");
         int a = 220;
         int b = 284;
         Calculadora instance = new Calculadora();
@@ -426,7 +426,7 @@ public class CalculadoraTest {
     
     @Test
     public void TNA02() {
-        System.out.println("sonAmigos");
+        System.out.println("TNA02");
         int a = 10;
         int b = 10;
         Calculadora instance = new Calculadora();
@@ -437,7 +437,7 @@ public class CalculadoraTest {
     
     @Test
     public void TNA03() {
-        System.out.println("sonAmigos");
+        System.out.println("TNA03");
         int a = -220;
         int b = -284;
         Calculadora instance = new Calculadora();
@@ -449,7 +449,7 @@ public class CalculadoraTest {
     //SUMA DE DIVISORES
     @Test
     public void TCSD01() {
-        System.out.println("sumaDivisores");
+        System.out.println("TCSD01");
         int x = 6;
         Calculadora instance = new Calculadora();
         int expResult = 6;
@@ -459,7 +459,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCSD02() {
-        System.out.println("sumaDivisores");
+        System.out.println("TCSD02");
         int x = -10;
         Calculadora instance = new Calculadora();
         int expResult = 1;
@@ -469,7 +469,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCSD03() {
-        System.out.println("sumaDivisores");
+        System.out.println("TCSD03");
         int x = 0;
         Calculadora instance = new Calculadora();
         int expResult = 1;
@@ -479,7 +479,7 @@ public class CalculadoraTest {
        
     @Test
     public void TCSD05() {
-        System.out.println("sumaDivisores");
+        System.out.println("TCSD05");
         int x = 7;
         Calculadora instance = new Calculadora();
         int expResult = 1;
@@ -489,7 +489,7 @@ public class CalculadoraTest {
     
     @Test
     public void TCSD06() {
-        System.out.println("sumaDivisores");
+        System.out.println("TCSD06");
         int x = 1;
         Calculadora instance = new Calculadora();
         int expResult = 1;
